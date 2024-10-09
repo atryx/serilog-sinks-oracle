613104fb660a76cbc8e443c4989b38cad602ef9e
atryx <lita.mihai@gmail.com>
2024-10-09 22:19:23 +0300

    Removed debug console log

--- sample/ConfigDemo/Program.cs
+++ sample/ConfigDemo/Program.cs
@@ -6,45 +6,37 @@ using Serilog.Sinks.Oracle;
 public static class Program
 {
     private const string _connectionStringName = "LogDatabase";
-    private const string _schemaName = "";
-    private const string _tableName = "Logs";
+    private const string _tableName = "Logs7";
 
     public static void Main()
     {
         try
         {
 
-        var configuration = new ConfigurationBuilder()
-            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-            .Build();
-        var columnOptionsSection = configuration.GetSection("Serilog:ColumnOptions");
-        var sinkOptionsSection = configuration.GetSection("Serilog:SinkOptions");
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .Build();
+            var columnOptionsSection = configuration.GetSection("Serilog:ColumnOptions");
+            var sinkOptionsSection = configuration.GetSection("Serilog:SinkOptions");
 
-        // New SinkOptions based interface
-        Log.Logger = new LoggerConfiguration()
-            .WriteTo.Oracle(
-                connectionString: _connectionStringName,
-                sinkOptions: new OracleSinkOptions
-                {
-                    TableName = _tableName,
-                    SchemaName = _schemaName,
-                    AutoCreateSqlTable = true
-                },
-                sinkOptionsSection: sinkOptionsSection,
-                appConfiguration: configuration,
-                columnOptionsSection: columnOptionsSection)
-            .CreateLogger();
-
-        Serilog.Debugging.SelfLog.Enable(msg => Console.Error.WriteLine(msg));
+            // New SinkOptions based interface
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Oracle(
+                    connectionString: _connectionStringName,
+                    sinkOptions: new OracleSinkOptions
+                    {
+                        TableName = _tableName,
+                        AutoCreateSqlTable = true
+                    },
+                    sinkOptionsSection: sinkOptionsSection,
+                    appConfiguration: configuration,
+                    columnOptionsSection: columnOptionsSection)
+                .CreateLogger();
 
 
-        Log.Information("Hello {Name} from thread {ThreadId}", Environment.GetEnvironmentVariable("USERNAME"), Environment.CurrentManagedThreadId);
-
-            Serilog.Debugging.SelfLog.Enable(msg => Console.Error.WriteLine(msg));
+            Log.Information("Hello {Name} from thread {ThreadId}", Environment.GetEnvironmentVariable("USERNAME"), Environment.CurrentManagedThreadId);
 
             Log.Warning("No coins remain at position {@Position}", new { Lat = 25, Long = 134 });
-
-            Serilog.Debugging.SelfLog.Enable(msg => Console.Error.WriteLine(msg));
 
             Log.CloseAndFlush();
 
